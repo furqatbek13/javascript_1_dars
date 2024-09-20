35cf3d4a4e2b28495d7077b5797576212dcabe60
furqatbek13 <furqatmatkarimov18@gmail.com>
2024-09-20 09:18:08 +0500

    Update script.js

--- script/script.js
+++ script/script.js
@@ -236,3 +236,7 @@
 // let S = 2 * (a*b + b*c + c*a);
 // console.log("Parallelopedning tola yuzi: " + S);
 
+//6. misol
+// let x = Math.floor(prompt('x sonni kiriting: '));
+// let y = (3*x**6) - (6*x**2) - 7;
+// console.log(y);
